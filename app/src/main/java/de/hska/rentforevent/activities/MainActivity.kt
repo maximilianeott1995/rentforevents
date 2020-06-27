a8070821c605710cba36d2d46e0fcc39ab264d83
Marcel Deufel <marcel.deufel@web.de>
2020-06-27 18:04:49 +0200

    Mock Daten bereitgestellt

--- app/src/main/java/de/hska/rentforevent/activities/MainActivity.kt
+++ app/src/main/java/de/hska/rentforevent/activities/MainActivity.kt
@@ -18,20 +18,20 @@ import java.util.*
 
 class MainActivity : LogoutActionBar(), AdapterView.OnItemSelectedListener {
     private val serviceProviders = arrayListOf(
-        ServiceProvider("Mike", "1.5/10", "Hamburg", "Unterhaltung", "150€/Stunde"),
-        ServiceProvider("Leon", "2.9/10", "Berlin", "Indie", "200€/Stunde"),
-        ServiceProvider("Andreas", "3.4/10", "Stuttgart", "Rap", "250€/Stunde"),
-        ServiceProvider("Martin", "4.6/10", "Karlsruhe", "Hip-Hop", "300€/Stunde"),
-        ServiceProvider("Flo", "5.3/10", "Bochum", "Catering", "350€/Stunde"),
-        ServiceProvider("Salva", "6.2/10", "Hamburg", "Indie", "450€/Stunde"),
-        ServiceProvider("Constantin", "7.5/10", "Heilbronn", "Rap", "500€/Stunde"),
-        ServiceProvider("Tim", "8.8/10", "Neckarsulm", "Hip-Hop", "100€/Stunde"),
-        ServiceProvider("Sebastian", "9.8/10", "Abstatt", "Schlager", "125€/Stunde"),
-        ServiceProvider("Stefan", "10/10", "Düsseldorf", "Rock", "275€/Stunde"),
-        ServiceProvider("Peter", "1.0/10", "Ettlingen", "Indie", "325€/Stunde"),
-        ServiceProvider("NOVA", "2.3/10", "Kiel", "Rap", "175€/Stunde"),
-        ServiceProvider("KEPA", "4.7/10", "Münster", "Hip-Hop", "370€/Stunde"),
-        ServiceProvider("JessBand", "8.3/10", "München", "Schlager", "500€/Stunde")
+        ServiceProvider("Marcels-Entertainment", "1.5/10", "Dresden", "Unterhaltung", "100€/Stunde"),
+        ServiceProvider("Andreas Putzservice", "2.9/10", "Köln", "Reinigung", "30€/Stunde"),
+        ServiceProvider("Andreas Transporter", "3.4/10", "Stuttgart", "Transport", "50€/Stunde"),
+        ServiceProvider("Martin's Putzservice", "4.6/10", "Karlsruhe", "Unterhaltung", "300€/Stunde"),
+        ServiceProvider("Florians Gaumenschmauß", "5.3/10", "Bochum", "Catering", "50€/Speiße"),
+        ServiceProvider("Horst Lieferservice", "6.2/10", "Stuttgart", "Transport", "60€/Stunde"),
+        ServiceProvider("Christians Transporter", "7.5/10", "Heilbronn", "Transport", "500€/Stunde"),
+        ServiceProvider("Lukas Putzservice", "8.8/10", "München", "Reinigung", "100€/Stunde"),
+        ServiceProvider("Sebastians-Comedy", "9.8/10", "Pforzheim", "Unterhaltung", "125€/Stunde"),
+        ServiceProvider("Steffens-Futterstube", "10/10", "Düsseldorf", "Catering", "10€/Speiße"),
+        ServiceProvider("Peters-Feinwarenkost", "1.0/10", "Hamburg", "Catering", "25€/Speiße"),
+        ServiceProvider("Reiners Clownshow", "2.3/10", "Kiel", "Unterhaltung", "175€/Stunde"),
+        ServiceProvider("Torstens Tranportservice", "4.7/10", "Ludwigsburg", "Transport", "370€/Stunde"),
+        ServiceProvider("Maxis Putzservice", "8.3/10", "München", "Reinigung", "50€/Stunde")
     )
 
     private val industries =
